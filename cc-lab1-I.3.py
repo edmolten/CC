8043ef50c32bc4e08dda29a6156a62214988f848
Eduardo Ramírez <eddoxx-v2.0@hotmail.es>
2015-04-10 23:53:04 -0300

    grafico

--- cc-lab1-I.3.py
+++ cc-lab1-I.3.py
@@ -5,10 +5,13 @@ def epsilon(fl):
     exp = floor(log(float(fl),2))
     return abs((2.0**(exp+1) - 2.0**(exp)) / 2.0**(52))
 
-print epsilon(1)
-
 # (b)
-import matplotlib.pyplot as plt
 
-plt.plot([1,2],[3,4])
-plt.show()
+from matplotlib import pyplot
+
+x = []
+for i in range(-50,51):
+    x.append(2.0**(i))
+
+pyplot.plot(x, [epsilon(i) for i in x])
+pyplot.show()    
