fcbd839269a8a5c1a205924773ada5c098b792f6
Eduardo Ramírez <eddoxx-v2.0@hotmail.es>
2015-04-12 17:31:46 -0300

    Comentarios de funciones y hermoseo general

--- cc-lab1-I.2.py
+++ cc-lab1-I.2.py
@@ -1,30 +1,42 @@
-
-import sys
 from decimal import Decimal
-def show(exp, x):
-    print exp, " = ", x
 
-# (b) Primer flotante fl(x) + e_mach no representable
+#Epsilon machine para flotantes segun estandar ISO 754
 e_mach = 2.0**(-52)
 
-def flNoRepresentable(x):
+#(b)
+def flNoRepresentable(i = 0.0, verbose = False):
+    """ Encuentra el menor flotante fl(x), buscando desde i, tal que fl(x) + e_mach no sea representable
+    i : Flotante inicial para busqueda, por defecto 0.0
+    verbose: True si se desea imprimir cada numero analizado, por defecto False
+    """
     while(1):
-        print Decimal(x)
-        anterior = x
-        x += e_mach
-        if(x == anterior):
-            return x
+        if verbose :
+            print Decimal(i)
+        anterior = i
+        i += e_mach
+        if(i == anterior):
+            return i
 
-print "El primer fl(x) tal que fl(x) + e_mach no es representable es: " + str(flNoRepresentable(1.9999999999999))
+#(c)
+def flRepresentable(i = 0.0, verbose = False):
+    """Encuentra el mayor flotante fl(x), buscando desde i, tal que fl(x) + e_mach / 2 sea representable
+    i : Flotante inicial para busqueda, por defecto 0.0
+    verbose : True si se desea imprimir cada numero analizado, por defecto False
+    """
+    while(1):
+        if verbose :
+            print Decimal(i)
+        anterior = i
+        i += e_mach/2
+        if i - anterior == 0:
+            return anterior - e_mach/2
 
+#Usando funciones
+print "Obteniendo menor flotante fl(x) + e_mach no representable"
+fl = flNoRepresentable(2 - 10 * e_mach)
+print "El menor fl(x) tal que fl(x) + e_mach no es representable es: ", Decimal(fl)
 raw_input("Presiona una tecla para continuar")
-            
-# (c) Primer flotante fl(x) + e_mach / 2  representable
-
-#def flRepresentable(x):
-    #while(1):
-      
-
-print "El primer fl(x) tal que fl(x) + e_mach / 2 es representable es: "# + str(flRepresentable(?))
-
-
+print "Obteniendo mayor flotante fl(x) + e_mach / 2  representable"
+fl = flRepresentable(1 - 10 * e_mach)
+print "El mayor fl(x) tal que fl(x) + e_mach / 2 es representable es: ", Decimal(fl)
+raw_input("Presiona una tecla para salir")
