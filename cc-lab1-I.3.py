82472a64dcd2c7a1d47e76d503a396bf30aa8c43
Eduardo Ramírez <eddoxx-v2.0@hotmail.es>
2015-04-12 19:15:59 -0300

    Arreglos en gráfico

--- cc-lab1-I.3.py
+++ cc-lab1-I.3.py
@@ -2,16 +2,25 @@ from math import log, floor, sin
 
 # (a)
 def epsilon(fl):
+    """Retorna el valor estimado de la diferencia entre fl y el
+    siguiente flotante representable
+    fl : flotante a analizar
+    """
     exp = floor(log(float(fl),2))
     return abs((2.0**(exp+1) - 2.0**(exp)) / 2.0**(52))
 
 # (b)
+#matplotlib no viene con Python 2.7 y tiene muchas dependencias.
+#No se asegura el correccto funcionamiento del siguiente codigo.
 
 from matplotlib import pyplot
 
-x = []
+fls = []
 for i in range(-50,51):
-    x.append(2.0**(i))
-
-pyplot.plot(x, [epsilon(i) for i in x])
+    fls.append(2.0**(i))
+pyplot.plot(fls, [epsilon(i) for i in fls])
+pyplot.ylabel('epsilon')
+pyplot.xlabel('flotante')
+pyplot.grid(True)
 pyplot.show()    
+
