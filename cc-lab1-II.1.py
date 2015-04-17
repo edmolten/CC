ce825dd701962314acfba2676c7bfba33d53214c
zkto <ian.zamorano7@gmail.com>
2015-04-17 19:52:50 -0300

    final ojala
    se calcularon las funciones alternativas al factor mavillano y se
    calcularon los errores relativos

--- cc-lab1-II.1.py
+++ cc-lab1-II.1.py
@@ -10,5 +10,25 @@ def tan(x):
 def y(x):
     return (1.0 - sec(x))/((tan(x))**2)
 
+def y1(x):
+    return (-1.0)/(1 + sec(x))
+
+def y2(x):
+    return (-cos(x))/(1 + cos(x))
+
+
 for t in range(1,21):
     print "y(10^(-" ,t,")) = " , Decimal(y(10.0**(-t)))
+
+for t in range(1,21):
+    print "y1(10^(-" ,t,")) = " , Decimal(y1(10.0**(-t)))
+
+    
+for t in range(1,21):
+    print "y2(10^(-" ,t,")) = " , Decimal(y2(10.0**(-t)))
+
+for t in range(1,21):
+    print "error relativo para t=",t," -> " , Decimal(abs(y(10.0**(-t)) - y1(10.0**(-t)))/abs(y1(10.0**(-t))))
+
+for t in range(1,21):
+    print "error relativo para t=",t," -> " , Decimal(abs(y(10.0**(-t)) - y2(10.0**(-t)))/abs(y2(10.0**(-t))))
