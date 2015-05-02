f3519be80c7fe6e737df2bb2ba00994147b72b48
zkto <ian.zamorano7@gmail.com>
2015-05-02 13:57:29 -0300

    de a pequeños pasos
    se agrego fixed point, revisar newton y secant method

--- lab2/cc_lab2_I.py
+++ lab2/cc_lab2_I.py
@@ -5,6 +5,7 @@ x = symbols("x")
 # Bisection method
 
 def Bisection(f,a,b,TOL):
+    f = sympify(f)
     if (f.subs(x,a)*f.subs(x,b) >= 0):
         print "Esta funcion no cumple con requisitos de Bisection Method"
     c = (a+b)/2
@@ -21,19 +22,36 @@ def Bisection(f,a,b,TOL):
 # Secant method --- revisar ---
 
 def SecantM(f,X_0,X_1,n):
+    f = sympify(f)
     pre = X_1
     past = X_0
     for i in range(n):
         fut = pre - (f.subs(x,pre) * (pre - past))/(f.subs(x,pre) - f.subs(x,past))
+        print fut
         if (pre == fut):
             return pre
         past = pre
         pre = fut
     return fut
 
-# Newto method
+
+# Fixed Point method
+
+def FixedPoint(f,X_0,n):
+    f = sympify(f)
+    past = X_0
+    for i in range(n):
+        pre = f.subs(x,past)
+        print pre
+        if (pre == past):
+            return pre
+        past = pre
+    return pre
+
+# Newton method
 
 def NewtonM(f,X_0,n):
+    f = sympify(f)
     past = X_0
     for i in range(n):
         pre = past - f.subs(x,past)/diff(f,x).subs(x,past)
