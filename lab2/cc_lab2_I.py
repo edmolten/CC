e1e8254ad1667adadb10825a58a450b3b415ea7a
zkto <ian.zamorano7@gmail.com>
2015-05-02 21:59:02 -0300

    Motedos modificados
    ahora se retorna un arreglo con los valores de cada iteracion

--- lab2/cc_lab2_I.py
+++ lab2/cc_lab2_I.py
@@ -5,6 +5,7 @@ x = symbols("x")
 # Bisection method
 
 def Bisection(f,a,b,TOL):
+    ar =[]
     f = sympify(f)
     if (f.subs(x,a)*f.subs(x,b) >= 0):
         print "Esta funcion no cumple con requisitos de Bisection Method"
@@ -12,63 +13,74 @@ def Bisection(f,a,b,TOL):
     c = (a+b)/2
     while((b-a)/2 > TOL):
         c = (a+b)/2
+        ar.append(c)
         if(f.subs(x,c) == 0):
-            return c
+            return ar
         if (f.subs(x,a)*f.subs(x,b) < 0):
             b=c
         else:
             a=c
-    return c
+    return ar
 
 # Secant method
 
 def SecantM(f,X_0,X_1,n):
     f = sympify(f)
+    a =[]
     pre = X_1
     past = X_0
     for i in range(n):
         fut = pre - (f.subs(x,pre) * (pre - past))/(f.subs(x,pre) - f.subs(x,past))
         print fut
-        if (f.subs(x,pre) == 0):
-            return pre
+        a.append(fut)
+        if (f.subs(x,fut) == 0):
+            return a
         past = pre
         pre = fut
-    return fut
+    return a
 
 
 # Fixed Point method
 
 def FixedPoint(f,X_0,n):
     f = sympify(f)
+    a =[]
     past = X_0
     for i in range(n):
         pre = f.subs(x,past)
         print pre
+        a.append(pre)
         if (pre == past):
-            return pre
+            return a
         past = pre
-    return pre
+    return a
 
 # Newton method
 
 def NewtonM(f,X_0,n):
     f = sympify(f)
+    a =[]
     past = X_0
     for i in range(n):
         pre = past - f.subs(x,past)/diff(f,x).subs(x,past)
         print pre
+        a.append(pre)
         if (f.subs(x,pre) == 0):
-            return pre
+            return a
         past = pre
-    return pre
+    return a
+
+# modified Newton method
 
 def NewtonMod(f,X_0,m,n):
     f = sympify(f)
+    a =[]
     past = X_0
     for i in range(n):
         pre = past - m * f.subs(x,past)/diff(f,x).subs(x,past)
         print pre
+        a.append(pre)
         if (f.subs(x,pre) == 0):
-            return pre
+            return a
         past = pre
-    return pre
+    return a
